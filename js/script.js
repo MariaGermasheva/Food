d05877b2821118fd92d2da4a88781050def95c99
Maria <germashevam@gmail.com>
2023-01-14 13:06:13 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    //Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -40,7 +42,62 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    //Timer
 
+    const deadline = '2023-05-20';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()), 
+        // указываем разницу между конечным временем и текущим (создавая new Date) 
+            days = Math.floor(t / (1000*60*60*24)), //выражение в скобках это расчет количества милисек в сутках
+            hours = Math.floor((t / (1000*60*60)) % 24), 
+            /* полученное количество часов в скобках делим на 24 (получаем остаток от деления), 
+            чтобы получить количсетво часов не более 24 ч в сутках */ 
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }; //т.к переменные выше сущ-ют только внутри функции, мы возвращаем их наружу в виде объекта
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); //вызываем ф-ю вручную, чтобы не было мигания верстки до момента запуска интервала
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            } //останавливаем обновление таймера, когда время до дедлайна вышло
+        }
+    }
+
+    setClock('.timer', deadline);
 
 });
 
